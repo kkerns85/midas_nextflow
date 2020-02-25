087375f2d6c791c1a94a52e97ec5f1237f6f86f0
Sam Minot <sam@fredhutch.org>
2020-02-25 12:24:11 -0800

    Run tests on both single- and paired-end datasets

--- run_test.sh
+++ run_test.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+echo "Processing paired-end data"
 NXF_VER=20.01.0 \
     nextflow \
     run \
@@ -10,8 +11,28 @@ NXF_VER=20.01.0 \
     midas_workflow.nf \
     --manifest test_data/manifest.paired.csv \
     --db db/ \
-    --output_folder test_output/ \
+    --output_folder test_output/paired/ \
     --species_cov 0.1 \
+    -with-docker ubuntu:18.04 \
+    -w work/ \
+    -process.executor local \
+    -with-report \
+    -with-trace \
+    -resume
+
+
+echo "Processing single-end data"
+NXF_VER=20.01.0 \
+    nextflow \
+    run \
+    -c nextflow.config \
+    -profile testing \
+    midas_workflow.nf \
+    --manifest test_data/manifest.single.csv \
+    --db db/ \
+    --output_folder test_output/single/ \
+    --species_cov 0.1 \
+    --single \
     -with-docker ubuntu:18.04 \
     -w work/ \
     -process.executor local \
